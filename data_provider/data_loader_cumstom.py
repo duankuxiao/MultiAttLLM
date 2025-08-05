2424378ea56efac2ad5219afb8821057da5158cf
duankuxiao <duankuxiao@gmail.com>
2025-08-05 12:19:15 +0900

    for MultiAttLLM paper

--- data_provider/data_loader_cumstom.py
+++ data_provider/data_loader_cumstom.py
@@ -17,6 +17,7 @@ class Dataset_cumstom(Dataset):
 
         self.num_train = configs.num_train
         self.num_test = configs.num_test
+        self.task_name = configs.task_name
 
         self.source_data_path = configs.source_data_path
         self.forecast_dim = configs.forecast_dim
@@ -47,24 +48,38 @@ class Dataset_cumstom(Dataset):
         self.__read_data__()
 
         self.enc_in = self.data_x.shape[-1]
-        self.tot_len = len(self.data_x) - self.seq_len - self.pred_len + 1
+        if 'forecast' in self.task_name:
+            self.tot_len = len(self.data_x) - self.seq_len - self.pred_len + 1
+        else:
+            self.tot_len = len(self.data_x) - self.seq_len + 1
+        if scale:
+            self.std_ = self.target_scaler.scale_
 
     def __getitem__(self, index):
-        s_begin = index % self.tot_len
-        # s_begin = (index // 24) * 24
-        s_end = s_begin + self.seq_len
-        r_begin = s_end - self.label_len
-        r_end = r_begin + self.label_len + self.pred_len
-        seq_x = self.data_x[s_begin:s_end, :]
-        seq_y = self.data_y[r_begin:r_end, :]
-        seq_x_mark = self.data_stamp[s_begin:s_end]
-        seq_y_mark = self.data_stamp[r_begin:r_end]
-        x_forecast = self.data_forecast[r_begin:r_end, :self.forecast_dim]
-
-        return seq_x, seq_y, seq_x_mark, seq_y_mark, x_forecast
+        if self.task_name == 'imputation':
+            s_begin = index % self.tot_len
+            # s_begin = (index // 24) * 24
+            s_end = s_begin + self.seq_len
+            seq_x = self.data_x[s_begin:s_end, :]
+            seq_x_mark = self.data_stamp[s_begin:s_end]
+            return seq_x, seq_x, seq_x_mark, seq_x_mark, seq_x
+        else:
+            s_begin = index % self.tot_len
+            # s_begin = (index // 24) * 24
+            s_end = s_begin + self.seq_len
+            r_begin = s_end - self.label_len
+            r_end = r_begin + self.label_len + self.pred_len
+            seq_x = self.data_x[s_begin:s_end, :]
+            seq_y = self.data_y[r_begin:r_end, :]
+            seq_x_mark = self.data_stamp[s_begin:s_end]
+            seq_y_mark = self.data_stamp[r_begin:r_end]
+            # x_forecast = self.data_forecast[r_begin:r_end, :self.forecast_dim]
+            x_forecast = self.data_forecast[r_begin:r_end, :self.forecast_dim]
+            return seq_x, seq_y, seq_x_mark, seq_y_mark, x_forecast
 
     def __len__(self):
         return len(self.data_x) - self.seq_len - self.pred_len + 1
+
 
     def inverse_transform(self, data):
         return self.target_scaler.inverse_transform(data)
@@ -72,9 +87,12 @@ class Dataset_cumstom(Dataset):
     def __read_data__(self):
         self.scaler = StandardScaler()
         self.target_scaler = StandardScaler()
-
-        df_source_domain = pd.read_csv(os.path.join(self.root_path, self.source_data_path))
-        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        try:
+            df_source_domain = pd.read_csv(os.path.join(self.root_path, self.source_data_path))
+            df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        except:
+            df_source_domain = pd.read_csv(os.path.join(self.root_path, self.source_data_path),encoding='SHIFT-JIS')
+            df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path),encoding='SHIFT-JIS')
 
         '''
         df_raw.columns: ['date', ...(other features), target feature]
@@ -86,15 +104,22 @@ class Dataset_cumstom(Dataset):
         if 'date' in cols:
             cols.remove('date')
 
-        for s in self.target:
-            if s in cols:
-                cols.remove(s)
+        if self.features == 'M' and self.target is None:
+            self.target = self.feature_cols
+        if self.target is not None:
+            for s in self.target:
+                if s in cols:
+                    cols.remove(s)
         df_raw = df_raw[['date'] + cols + self.target]
         df_source_domain = df_source_domain[['date'] + cols + self.target]
 
         num_vali = len(df_raw) - self.num_train - self.num_test
-        border1s = [0, self.num_train - self.seq_len, len(df_raw) - self.num_test - self.seq_len]
-        border2s = [self.num_train, self.num_train + num_vali, len(df_raw)]
+        if 'forecast' in self.task_name:
+            border1s = [0, self.num_train - self.seq_len, len(df_raw) - self.num_test - self.seq_len]
+            border2s = [self.num_train, self.num_train + num_vali, len(df_raw)]
+        else:
+            border1s = [0, self.num_train, len(df_raw) - self.num_test]
+            border2s = [self.num_train, self.num_train + num_vali, len(df_raw)]
 
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
@@ -121,7 +146,8 @@ class Dataset_cumstom(Dataset):
             train_data = df_data_source_domain[border1s[0]:border2s[0]]
             self.scaler.fit(train_data.values)
             data = self.scaler.transform(df_data.values)
-            self.target_scaler.fit(df_target_source_domain.values)
+            df_target_data = df_target_source_domain[border1s[0]:border2s[0]]
+            self.target_scaler.fit(df_target_data.values)
 
         else:
             data = df_data.values
@@ -141,69 +167,12 @@ class Dataset_cumstom(Dataset):
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
 
-
         if self.features == 'S':
             self.data_x = data[border1:border2, -1:]
             self.data_y = data[border1:border2, -1:]
         else:
             self.data_x = data[border1:border2, :len(self.feature_cols)]
             self.data_y = data[border1:border2, -len(self.target):]
-
         self.data_forecast = data[border1:border2, :self.forecast_dim]
         self.data_stamp = data_stamp
 
-
-if __name__ == '__main__':
-    from utils.tools import heatmap
-    # folder = r'D:\Time-LLM-main\dataset\pv'
-    # data_file = 'PV_hour.csv'
-    # data = pd.read_csv(os.path.join(folder, data_file),index_col=0)
-    # print(data.columns)
-    # # data = data[['Temperature', 'Relative_humidity',  'Wind_speed', 'Global_horizontal_irradiance', 'System_price', 'Sell_volume', 'Buy_volume', 'Total_volume',
-    # #                      'Sell_volume_block_orders', 'Sell_volume_contracted_block_orders', 'Buy_volume_block_orders', 'Buy_volume_contracted_block_orders',  'Price']]
-    # data = data[['Temperature','Relative_humidity','Precipitation','Dew_point','Vapor_pressure','Wind_speed','Sunshine_duration','Snowfall','Global_horizontal_irradiance']]
-    # data_path = 'pv'
-    # heatmap(data,data_path)
-
-    folder = r'D:\Time-LLM-main\dataset\electricity'
-    # data_file = 'kyushu.csv'
-    # data = pd.read_csv(os.path.join(folder, data_file), index_col=0)
-    # data_path = 'kyushu_electricity'
-    # heatmap(data, data_path)
-
-    # data_file = 'spot_index.csv'
-    # data = pd.read_csv(os.path.join(folder, data_file))
-    # df_repeated = data.loc[data.index.repeat(24)].reset_index(drop=True)
-    # df_repeated.to_csv(os.path.join(folder, data_file[:-4] + '_24h.csv'), index=False)
-    # print(df_repeated)
-
-    # data_file = 'intraday_30min.csv'
-    # df = pd.read_csv(os.path.join(folder, data_file))
-    # data = df.iloc[::2, :]
-    # print(data)
-    # data.to_csv(os.path.join(folder, data_file[:-4] + '_1h.csv'))
-
-    folder = r'D:\Time-LLM-main\dataset\electricity\hokkaido'
-    file_names = [f'sup_dem_results_{year}_{quarter}q.csv' for year in range(2019, 2024) for quarter in range(1, 5)]
-    file_names = file_names[3:-1]  # 限制到 '23_3q'
-
-
-    # 读取并合并所有文件
-    dfs = []
-    for file_name in file_names:
-        file_path = os.path.join(folder, file_name)
-        # # 读取txt文件为DataFrame，假设是逗号分隔
-        # df = pd.read_csv(os.path.join(folder,file_name[:-4]+'.txt'), delimiter=',', encoding='utf-8')
-        # output_csv_path = file_path
-        # # 将DataFrame保存为csv文件
-        # df.to_csv(output_csv_path, index=False, encoding='utf-8')
-
-        if os.path.exists(file_path):  # 检查文件是否存在
-            df = pd.read_csv(file_path,encoding='SHIFT-JIS').iloc[3:,:]
-            dfs.append(df)
-
-    # 合并所有DataFrame
-    combined_df = pd.concat(dfs, ignore_index=True)
-
-    # 保存为一个新的csv文件
-    combined_df.to_csv(os.path.join(folder,'hokkaido.csv'), index=False, encoding='SHIFT-JIS')
