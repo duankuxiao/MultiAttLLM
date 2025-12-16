eaf5bcb23e662f6861e1464941b7fab487b8b922
duankuxiao <duankuxiao@gmail.com>
2025-12-16 16:13:12 +0900

    Redundant code removed

--- configs/electricity_configs.py
+++ configs/electricity_configs.py
@@ -1,11 +1,9 @@
 from configs.common_configs import args
 
-args.model_comment = 'electricity'  # 能源供应
+args.model_comment = 'electricity'
 args.data = 'electricity'
 args.root_path = './dataset/electricity'
 args.data_path = 'tokyo.csv'
-# args.data_path = 'kyushu.csv'
-# args.data_path = 'Texas.csv'
 
 args.source_data_path = args.data_path
 
@@ -15,10 +13,6 @@ args.feature_cols = ['Electricity','Renewable_energy', 'Nuclear', 'Coal', 'Hydro
                      'Global_horizontal_irradiance']
 args.target = ['Electricity', 'Renewable_energy', 'Coal']
 
-if args.data_path == 'Texas.csv':
-    args.feature_cols = ['COAST', 'EAST', 'FWEST', 'NORTH', 'NCENT', 'SOUTH', 'SCENT', 'WEST', 'REGDN', 'REGUP', 'RRS', 'NSPIN', 'WIND_ACTUAL_SYSTEM_WIDE',
-                         'SOLAR_ACTUAL_SYSTEM_WIDE', 'LZ_AEN', 'LZ_CPS', 'LZ_HOUSTON', 'LZ_LCRA', 'LZ_NORTH', 'LZ_RAYBN', 'LZ_SOUTH', 'LZ_WEST']
-    args.target = args.feature_cols
 args.num_train = 8760*2+24
 args.num_test = 8760
 args.seq_len = 72
@@ -29,18 +23,4 @@ args.enc_in = 22
 args.dec_in = 22
 args.c_out = 1
 args.scale = True
-
-
-# ['Electricity', 'Nuclear', 'Coal', 'Hydro', 'Geothermal', 'Biomass',
-#        'Solar', 'Solar_curtailment', 'Wind', 'Wind_ccurtailment',
-#        'Water_pumping', 'Interconnection', 'Total', 'Temperature',
-#        'Relative_humidity', 'Precipitation', 'Dew_point', 'Vapor_pressure',
-#        'Wind_speed', 'Sunshine_duration', 'Snowfall',
-#        'Global_horizontal_irradiance']
-
-if __name__ == '__main__':
-    import pandas as pd
-    import os
-    data = pd.read_csv(os.path.join(r'D:\Time-LLM-main\dataset\electricity', 'Texas.csv'))
-    print(data.columns)
 
--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,18 +1,5 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4
 from torch.utils.data import DataLoader
 from data_provider.data_loader_cumstom import Dataset_cumstom, Dataset_classification
-
-data_dict = {
-    'ETTh1': Dataset_ETT_hour,
-    'ETTh2': Dataset_ETT_hour,
-    'ETTm1': Dataset_ETT_minute,
-    'ETTm2': Dataset_ETT_minute,
-    'ECL': Dataset_Custom,
-    'Traffic': Dataset_Custom,
-    'Weather': Dataset_Custom,
-    'm4': Dataset_M4,
-}
-
 
 def data_provider(args, flag):
     Data = Dataset_cumstom
@@ -23,43 +10,24 @@ def data_provider(args, flag):
     shuffle_flag = False if (flag == 'test' or flag == 'TEST') else True
     batch_size = 1 if (flag == 'test' or flag == 'TEST') else args.batch_size
 
-    if args.task_name == 'classification':
-        drop_last = False
-        data_set = Dataset_classification(
-            configs=args,
-            root_path=args.root_path,
-            data_path=args.data_path,
-            flag=flag,
-        )
-
-        data_loader = DataLoader(
-            data_set,
-            batch_size=batch_size,
-            shuffle=shuffle_flag,
-            num_workers=args.num_workers,
-            drop_last=drop_last,
-        )
-        return data_set, data_loader
-
-    else:
-        data_set = Data(
-            configs=args,
-            root_path=args.root_path,
-            data_path=args.data_path,
-            flag=flag,
-            size=[args.seq_len, args.label_len, args.pred_len],
-            features=args.features,
-            target=args.target,
-            timeenc=timeenc,
-            freq=freq,
-            percent=percent,
-            scale=args.scale,
-            seasonal_patterns=args.seasonal_patterns,
-        )
-        data_loader = DataLoader(
-            data_set,
-            batch_size=batch_size,
-            shuffle=shuffle_flag,
-            num_workers=args.num_workers,
-            drop_last=drop_last)
-        return data_set, data_loader
+    data_set = Data(
+        configs=args,
+        root_path=args.root_path,
+        data_path=args.data_path,
+        flag=flag,
+        size=[args.seq_len, args.label_len, args.pred_len],
+        features=args.features,
+        target=args.target,
+        timeenc=timeenc,
+        freq=freq,
+        percent=percent,
+        scale=args.scale,
+        seasonal_patterns=args.seasonal_patterns,
+    )
+    data_loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=shuffle_flag,
+        num_workers=args.num_workers,
+        drop_last=drop_last)
+    return data_set, data_loader
